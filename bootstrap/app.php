8f5ee943276e6539fc3f30888d89bffe821f2c37
Michael <billyston@gmail.com>
2025-04-27 20:50:06 +0000

    stub, ApiExceptions done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,19 +2,37 @@
 
 declare(strict_types=1);
 
+use App\Exceptions\ApiExceptionRenderer;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+    )->withMiddleware(
+        function (
+            Middleware $middleware
+        ): void {
+            //
+        })->withExceptions(
+        function (
+            Exceptions $exceptions
+        ): void {
+            $exceptions->render(
+                using: function (
+                    Throwable $throwable, Request $request) {
+                    if ($request->expectsJson()) {
+                        return new ApiExceptionRenderer(
+                            exception: $throwable,
+                            request: $request,
+                        )->render();
+                    }
+
+                    return null;
+                },
+            );
+        })->create();
